508577d434a154fe4cd9e40177493dbd6a40db78
Rodolfo Fernandez <r.fernander90@gmail.com>
2018-05-20 18:50:11 +0000

    added comparisonoperators.php

---
+++ math.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This is my php website</title>
+    </head>
+</html>
+    
+    <body>
+        <center>
+            
+            <h1>
+                <?php
+                
+                 $number_1 = 41;
+                 $number_2 = 4; 
+                 
+                 echo $number_1 + $number_2 . "</br>";
+                 echo $number_1 / $number_2 . "</br>";
+                 echo $number_1 - $number_2 . "</br>";
+                 echo "++ to increment one --> " . ++$number_1 . "</br>";
+                 echo "-- to decrement one --> " . --$number_1 . "</br>";
+                 
+                ?> 
+            </h1>
+   
+        </center>
+
+    </body>
+
+
